53d28a23826b85c7a05ad832fd0a0b4261faf9d3
koteshwartgt <koteshwar.s@thoughtgreen.com>
2023-06-17 12:52:52 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ sys.path.append(project_root)
 from exception import CustomException
 from logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,24 +22,31 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
     
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
         
         for model_key,model_value in models.items():
             logging.info(f"The model name is '{model_key}'")
+            para = param[model_key]
+            gs = GridSearchCV(model_value,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model_value.set_params(**gs.best_params_)
             model_value.fit(x_train,y_train)
+            #model_value.fit(x_train,y_train)
             y_train_pred = model_value.predict(x_train)
             y_test_pred = model_value.predict(x_test)
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
-            #print('the test_model_score-------->',test_model_score,model_key)
+            print('the test_model_score-------->',test_model_score,model_key)
             report[model_key] = test_model_score            
-            
+        print('the report is---------->',report)   
         return report
             
-    except:
-        pass
+    except Exception as e:
+        raise CustomException(e,sys)
+        
     
     
     
