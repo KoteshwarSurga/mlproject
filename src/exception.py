5d179107fcca49e1b789ba7ac4c2361c40060997
koteshwartgt <koteshwar.s@thoughtgreen.com>
2023-06-17 16:36:30 +0530

    predict pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
-import sys
-from logger import logging
+import sys,os
+# project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(project_root)
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,12 @@ import sys
 
 import numpy as np
 import pandas as pd
-import dill
+import dill,pickle
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_root)
-from exception import CustomException
-from logger import logging
+# project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(project_root)
+from src.exception import CustomException
+from src.logger import logging
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -20,6 +20,17 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+    
     
     
 def evaluate_model(x_train,y_train,x_test,y_test,models,param):
